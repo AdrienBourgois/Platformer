ff6f1a48194990b5b8f301a6dcbb189018114a73
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2015-11-20 16:44:10 +0100

    Calcul distance point to plane (some bugs)

--- include/maths/vector.h
+++ include/maths/vector.h
@@ -6,9 +6,21 @@
 namespace id {
 namespace maths {
 
+struct Vector4
+{
+    float val[4];
+    
+    auto operator+(Vector4 const vector) 	-> Vector4;
+    auto operator*(float& val) const 		-> Vector4;
+    auto operator*=(float& val) 			-> Vector4&;	
+};
+
 struct Vector3
 {
 	float val[3];
+
+    Vector3() = default;
+    Vector3(float, float, float);
 	
 	auto operator+(Vector3 const vector) 	-> Vector3;
 	auto operator*(float& val) const 		-> Vector3;
@@ -17,19 +29,14 @@ struct Vector3
 		
 	auto dotProduct(Vector3 vec) -> float;
 	auto crossProduct(Vector3 vec) -> Vector3;
+    auto cartEquation(Vector3 vec1, Vector3 vec2, Vector3 vec3) -> Vector4;
+    auto distancePointToPlan(Vector3 point, Vector4 plan) -> float;
 
 	auto getNormalized() const 				-> Vector3;
 	auto norm() -> float;
 };
 
-struct Vector4
-{
-	float val[4];
-	
-	auto operator+(Vector4 const vector) 	-> Vector4;
-	auto operator*(float& val) const 		-> Vector4;
-	auto operator*=(float& val) 			-> Vector4&;	
-};
+
 
 } // namespace maths
 
--- src/maths/vector.cpp
+++ src/maths/vector.cpp
@@ -5,6 +5,13 @@
 namespace id {
 namespace maths {
 
+Vector3::Vector3(float val0, float val1, float val2)
+{
+    this->val[0] = val0;
+    this->val[1] = val1;
+    this->val[2] = val2;
+}
+
 auto Vector3::operator+(Vector3 const vector) -> Vector3
 {
 	return { this->val[0] + vector.val[0], this->val[1] + vector.val[1], this->val[2] + vector.val[2] };
@@ -14,8 +21,6 @@ auto Vector3::operator*(float& val) const -> Vector3
 {
 	return { this->val[0] * val, this->val[1] * val, this->val[2] * val };
 }
-
-
 
 auto Vector3::operator*=(float& val) -> Vector3&
 {
@@ -64,12 +69,36 @@ auto Vector3::norm() -> float
 	
 }
 
+auto Vector3::cartEquation(Vector3 vec1, Vector3 vec2, Vector3 vec3) -> Vector4
+{
+    Vector3 AB = vec2 - vec1;
+    Vector3 BC = vec3 - vec1;
 
+    std::cout << AB.val[0] << std::endl;
+    std::cout << AB.val[1] << std::endl;
+    std::cout << AB.val[2] << std::endl;
+    std::cout << BC.val[0] << std::endl;
+    std::cout << BC.val[1] << std::endl;
+    std::cout << BC.val[2] << std::endl;
 
+    Vector3 M = AB.crossProduct(BC);
 
+    int d = -((vec1.val[0] * M.val[0]) + (vec1.val[1] * M.val[1]) + (vec1.val[1] * M.val[1]));
 
+    Vector4 equation;
 
+    equation.val[0] = M.val[0];
+    equation.val[1] = M.val[1];
+    equation.val[2] = M.val[2];
+    equation.val[3] = d;
 
+    return equation;
+}
+
+auto Vector3::distancePointToPlan(Vector3 point, Vector4 plan) -> float
+{
+    return (abs(plan.val[0] * point.val[0] + plan.val[1] * point.val[1] + plan.val[2] * point.val[2] + plan.val[3]) / sqrt(pow(plan.val[0], 2) + pow(plan.val[1], 2) + pow(plan.val[2], 2)));
+}
 
 auto Vector4::operator+(Vector4 const vector) -> Vector4
 {
