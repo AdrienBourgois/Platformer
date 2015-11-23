f21a81a7db28de2d88a4ac64c3ab816ed485844b
Tristan Naudon <t.naudon@student.isartdigital.com>
2015-11-23 09:54:28 +0100

    add sphere.cpp to create sphere..

---
+++ src/maths/sphere.cpp
@@ -0,0 +1,13 @@
+#include "maths/vector.h"
+#include "maths/sphere.h"
+
+namespace id {
+namespace maths {
+
+Sphere::Sphere(Vector3 center, float rayon)
+:center(center), rayon(rayon)
+{
+
+}
+} // namespace maths
+} // namespace id
